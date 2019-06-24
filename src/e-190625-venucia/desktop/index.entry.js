6326f121bce6f133fbd75a9779810aa45ff4e8d0
zhangzongxiang <zhangzongxiang@zzx-macbook.local>
2019-06-24 18:15:22 +0800

    animate

--- src/e-190625-venucia/desktop/index.entry.js
+++ src/e-190625-venucia/desktop/index.entry.js
@@ -2,7 +2,7 @@ import 'core-js';
 import $ from '@/lib/src/jquery-3.4.1.min.js';
 import './assets/styles/index';
 
-const elementIds = ['banner', 'design'];
+const elementIds = ['design'];
 const elements = elementIds.reduce((res, id) => {
   const el = document.getElementById(id);
   res[id] = {
@@ -17,7 +17,6 @@ $(window).scroll(() => {
   const windiwScrollTop = $(window).scrollTop();
   for (const key of Object.keys(elements)) {
     if (!elements[key].ready && elements[key].offsetTop >= windiwScrollTop) {
-      console.log('element-in')
       elements[key].el.addClass('element-in');
       elements[key].ready = true;
     }
